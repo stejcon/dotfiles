1babc6d7693af1d4ee30a8ebab892913b174cf80
Stephen Condon <stephen.condon2@gmail.com>
2022-04-20 23:58:10 +0100

    Add neovim dotfiles

---
+++ .config/nvim/lua/user/completion.lua
@@ -0,0 +1,28 @@
+local cmp = require 'cmp'
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+cmp.setup {
+	mapping = {
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-e>"] = cmp.mapping.close(),
+		["<C-y>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
+		["<C-space>"] = cmp.mapping.complete(),
+	},
+
+	sources = {
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "buffer", keyword_length = 5 },
+	},
+
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+}
